2da141fdfef7b89967ac51354d33a09a08e8bcfc
nani14ing <nani14ing@gmail.com>
2022-06-01 16:49:47 +0530

    conflict add

--- src/test/java/com/sampleTest/SampleTest1.java
+++ src/test/java/com/sampleTest/SampleTest1.java
@@ -7,8 +7,12 @@ public class SampleTest1 {
 	@Test
 	public void SampleTest1() {
 		System.out.println("Sample");
+<<<<<<< HEAD
 		//changes
 //person2 updated for conflict
+=======
+//conflict by person1
+>>>>>>> dab4e7afe648fd4c17029e5c1167773c3a6498bb
 	}
 	
 }
